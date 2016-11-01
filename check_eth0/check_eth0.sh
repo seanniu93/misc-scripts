0ec986411178afe536f2a04050027cd348540a28
Sean Niu <seanniu93@gmail.com>
2016-11-01 11:46:20 -0700

    Change ping target to default gateway
    This script should check if the network interface is up by checking if
    the device is connected to the router, not by checking if it is
    connected to the internet. That way, it doesn't restart the interface
    when the router loses connection.

--- check_eth0/check_eth0.sh
+++ check_eth0/check_eth0.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 #
-# This is a cron script that checks if it is connected to the Internet by
+# This is a cron script that checks if it is connected to the local network by
 # pinging an address. If it detect that it is disconnected, it will restart
 # the dhcpcd service.
 #
 # Example crontab entry:
 # * * * * * /home/pi/cron/check_eth0.sh >> /var/log/cron/check_eth0.log 2>&1
 
-ping -q -w 10 -c 1 8.8.8.8 > /dev/null 2>&1
+DEFAULT_GATEWAY='10.0.1.1'
+
+ping -q -w 10 -c 1 $DEFAULT_GATEWAY > /dev/null 2>&1
 
 # $? is the exit status
 if [ $? -ne 0 ]; then
